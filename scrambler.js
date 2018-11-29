836458224b428139e6ae1bf1aaef899b180e1dbd
Devin Sova <devin.m.sova@gmail.com>
2018-11-29 17:48:12 -0500

    Started Alg and Created Helpers

--- scrambler.js
+++ scrambler.js
@@ -1,12 +1,29 @@
 function scrambleWord(word) {
-    if(typeof word != 'string')
-        return null
-    
-    //Can't scramble words of length 2 or less.
-    if(word.length <= 2)
-        return word
-
     //Using a modern version Fisher-Yates Shuffle known as the Knuth Shuffle.
-    //We are only shuffling letters at position 1 to n-1. (For String 0...n)
+    //We are only shuffling letters at position 1 to n-2. (For String 0...n-1 and n = length)
+    let n = word.length
+    for(i = 0; i < n-2; i++) {
+        j = getRandomInteger(n-1, 0)
+        
+    }
+ }
+ 
+function pickOutWords(text) {
+    //Can't scramble words of length 3 or less.
+    //Only scramble words with Alphabetical letters.
+    //Regex is used to check
+
+    //text.match(/([A-z]+[A-z][A-z]+)/))
     //TODO: Implement
 }
+
+
+ //Helper Methods
+ /**
+ * @param {number} max      Max Value for Integer.
+ * @param {number} shift    Value for Integer.
+ * @return {number}         Integer between 0 and max plus shift
+ */
+ function getRandomInteger(max, shift) {
+     return Math.floor(Math.random() * Math.floor(max)) + shift
+ }
