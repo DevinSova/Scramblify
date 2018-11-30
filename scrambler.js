63ca169f71f0a89ea72d1fba495733494ab08f1c
Devin Sova <devin.m.sova@gmail.com>
2018-11-30 10:32:08 -0500

    Fixed Bugs

--- scrambler.js
+++ scrambler.js
@@ -1,20 +1,28 @@
 function scrambleWord(word) {
     //Using a modern version Fisher-Yates Shuffle known as the Knuth Shuffle.
-    //We are only shuffling letters at position 1 to n-2. (For String 0...n-1 and n = length)
+    //We are only shuffling letters at position 1 to n-2. (For String 0...n-1 (n = length))
     let n = word.length
+    var wordAsArray = word.split('')
     for(i = 0; i < n-2; i++) {
-        j = getRandomInteger(n-1, 0)
-        
+        j = getRandomInteger(n, 0)
+        swap(i, j, wordAsArray)
+        console.log(wordAsArray)
     }
+    word = wordAsArray.join('')
+    console.log(word)
  }
  
 function pickOutWords(text) {
     //Can't scramble words of length 3 or less.
     //Only scramble words with Alphabetical letters.
     //Regex is used to check
-
-    //text.match(/([A-z]+[A-z][A-z]+)/))
-    //TODO: Implement
+    var words = text.split(' ')
+    for(word in words) {
+        if(word.match(/([A-z]+[A-z][A-z]+)/))
+            word = scrambleWord(word)
+    }
+    console.log(words.join(' '))
+    text = words.join(' ')
 }
 
 
@@ -26,4 +34,15 @@ function pickOutWords(text) {
  */
  function getRandomInteger(max, shift) {
      return Math.floor(Math.random() * Math.floor(max)) + shift
+ }
+
+ /**
+ * @param {number}  p1      Position 1
+ * @param {number}  p2      Position 2
+ * @param {array}   array   Array to do Swap
+ */
+ function swap(p1, p2, array) {
+     var temp = array[p1] 
+     array[p1] = array[p2]
+     array[p2] = temp
  }
