bcf3ef730d86337e412a24f6c3d0552bdd870015
Devin Sova <devin.m.sova@gmail.com>
2018-12-03 16:10:53 -0500

    Working Correctly!!

--- substitute.js
+++ substitute.js
@@ -1,9 +1,21 @@
 function updateText(node) {
-    if(node.nodeType == Node.TEXT_NODE) {
+    if(node.nodeType === Node.TEXT_NODE) {
+
+        //AvoiD Scrambling user input
+        if(node.parentNode && node.parentNode.nodeName === 'TEXTAREA') {
+            return
+        }
+
         let content = node.textContent
+        console.log(content)
         content = scrambleText(content)
+        console.log(content)
         node.textContent = content
     }
-}
 
+    //Recursively Call on Child Nodes
+    for(let i = 0; i < node.childNodes.length; i++) {
+        updateText(node.childNodes[i])
+    }
+}
 updateText(document.body)
